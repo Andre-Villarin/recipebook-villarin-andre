bfadf83b715f2823397cf42d7dffde69fc0ec69c
Andre-Villarin <andre.villarin@gmail.com>
2025-02-15 22:26:15 +0800

    Update settings.py

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -19,10 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4i!i8a%^z#x&k9h(0aha0k(wrmwi&7x9qmg^ff4hks9=0nclcq'
+SECRET_KEY = 'REMOVED'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
