e1b1e9fbc0d07e25ee715e9e970575f4efba3238
Andre-Villarin <andre.villarin@gmail.com>
2025-03-02 01:56:12 +0800

    Added admin panels for each model.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from ledger import models
+from .models import Ingredient, Recipe, RecipeIngredient
 
-# Register your models here.
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+    list_display = ('recipe', 'ingredient', 'quantity')
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
