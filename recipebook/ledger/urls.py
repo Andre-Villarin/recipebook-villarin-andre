1dd13bc3b54b2cd35059b01ba97bfc63e4da01bc
Andre-Villarin <andre.villarin@gmail.com>
2025-03-02 01:41:50 +0800

    Added dynamic formatting for views.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from ledger import views
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect('/recipes/list/')),
+    # path('', lambda request: HttpResponseRedirect('/recipes/list/')),
     path("recipes/list/", views.recipe_list, name = "recipe_list"),
     path("recipe/<int:id>/", views.recipe_info, name = "recipe_detail"),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from.models import Recipe
 
-def recipe_list(request): 
-	return render(request, "recipe_list.html") 
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, "recipe_list.html", {"recipes": recipes}) 
 
 def recipe_info(request, id):
      recipe = Recipe.objects.get(id=id)
