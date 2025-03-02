57b10fe1b166a468f90146b184512e91f5b29c1f
Andre-Villarin <andre.villarin@gmail.com>
2025-03-02 22:59:44 +0800

    Removed redundant URL pattern.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from ledger import views
 
 urlpatterns = [
-    # path('', lambda request: HttpResponseRedirect('/recipes/list/')),
     path("recipes/list/", views.recipe_list, name = "recipe_list"),
     path("recipe/<int:id>/", views.recipe_info, name = "recipe_info"),
     ]
