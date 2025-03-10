321f81ba1569bb0a155b86c39f1f7d6bc45fb5bb
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-03-11 00:13:28 +0800

    Restricted recipe detail page to logged-in users only.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from.models import Recipe
+from django.contrib.auth.decorators import login_required
 
 def list_redirect(request):
     return redirect("recipe_list")
@@ -8,6 +9,7 @@ def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, "recipe_list.html", {"recipes": recipes}) 
 
+@login_required
 def recipe_info(request, id):
      recipe = Recipe.objects.get(id=id)
      return render(request, "recipe_info.html", {"recipe": recipe})
