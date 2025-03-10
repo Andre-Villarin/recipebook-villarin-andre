776f80e7ec8cf1307261d2b5493fdd1c016675a7
Andre-Villarin-ADMU <diniel.villarin@student.ateneo.edu>
2025-03-11 00:34:40 +0800

    Fixed redirecting issues when user logs in/out.

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -130,3 +130,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect settings for user sessions
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
